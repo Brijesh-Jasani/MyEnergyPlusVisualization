7b3607638d7f4debcc866cbcb32f061544cef221
Brijesh Jasani <81949381+Brijesh-Jasani@users.noreply.github.com>
2022-08-26 18:27:59 +0530

    Delete About.py

--- About.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("About")
-st.markdown(" Do you like this app?")
-st.markdown(" Any suggestions?")
-st.write(" Please give your feedback here [Gmail](brijesh221097@gmail.com)")
-st.write("\U0001F4E1 you can contact me here \U0001F603	,[LinkedIn](https://www.linkedin.com/in/brijesh-jasani/),")
